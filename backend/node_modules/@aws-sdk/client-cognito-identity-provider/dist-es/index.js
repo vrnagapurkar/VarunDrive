9dd798f065c319ea1b7bb634749f26292e9a4029
Varun Nagapurkar <varunnagapurkar@Varuns-MacBook-Pro.local>
2025-12-14 16:58:14 -0600

    completed

--- backend/node_modules/@aws-sdk/client-cognito-identity-provider/dist-es/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./CognitoIdentityProviderClient";
-export * from "./CognitoIdentityProvider";
-export * from "./commands";
-export * from "./pagination";
-export * from "./models/enums";
-export * from "./models/errors";
-export { CognitoIdentityProviderServiceException } from "./models/CognitoIdentityProviderServiceException";
